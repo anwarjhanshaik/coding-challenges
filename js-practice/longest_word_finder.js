1e3fc503d56e62af66abd32f6933c9339f8f9f6e
Shaik Sajida <anwarjhanshaik@gmail.com>
2026-07-23 11:10:34 +0530

    Refactor longest word finder for O(1) space complexity
    Refactored findLongestWordLength function for improved space complexity.

--- js-practice/longest_word_finder.js
+++ js-practice/longest_word_finder.js
@@ -1,4 +1,4 @@
-//Longest Word Finder
+//Longest Word Finder O(N) TIME AND SPACE COMPLEXITY 
 function findLongestWordLength(sentence) {
   const splitted_sentence = sentence.split(" ");
   let maxLength = 0;
@@ -13,3 +13,20 @@ console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"
 console.log(findLongestWordLength("May the force be with you"));
 console.log(findLongestWordLength("Google do a barrel roll"));
 console.log(findLongestWordLength("Googling do a barrel roll"));
+
+//Longest Word Finder O(N) TIME O(1) SPACE COMPLEXITY 
+function findLongestWordLength(sentence) {
+  maxLength = 0;
+  currentLength = 0;
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " ") {
+      if (currentLength > maxLength) {
+        maxLength = currentLength;
+      }
+      currentLength = 0;
+    } else {
+      currentLength++;
+      }
+  }
+  return maxLength > currentLength ? maxLength : currentLength;
+}
